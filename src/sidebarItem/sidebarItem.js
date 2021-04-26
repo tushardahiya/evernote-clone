e6c3cdd81adcfb10834492074024962420c9c462
tushardahiya <tushardahiya84@gmail.com>
2021-04-26 11:19:52 +0530

    worked on sidebaritem component

--- src/sidebarItem/sidebarItem.js
+++ src/sidebarItem/sidebarItem.js
@@ -7,11 +7,41 @@ import DeleteIcon from "@material-ui/icons/Delete";
 import { removeHTMLTags } from "../helpers";
 
 class SidebarItemComponent extends Component {
-  constructor() {
-    super();
-  }
+  // n is note and i is index
+  selectNote = (n, i) => this.props.selectNote(n, i);
+
+  deleteNote = (note) => {
+    if (window.confirm(`Are you sure you want to delete : ${note.title}`)) {
+      this.props.deleteNote(note);
+    }
+  };
+
   render() {
-    return <div>hello from sidebar Item</div>;
+    const { index, note, classes, selectedNoteIndex } = this.props;
+
+    return (
+      <div key={index}>
+        <ListItem
+          className={classes.ListItem}
+          selected={selectedNoteIndex === index}
+          alignItems="flex-start"
+        >
+          <div
+            className={classes.textSection}
+            onClick={() => this.selectNote(note, index)}
+          >
+            <ListItemText
+              primary={note.title}
+              secondary={removeHTMLTags(note.body.substring(0, 30)) + "..."}
+            ></ListItemText>
+          </div>
+          <DeleteIcon
+            onClick={() => this.deleteNote(note)}
+            className={classes.DeleteIcon}
+          ></DeleteIcon>
+        </ListItem>
+      </div>
+    );
   }
 }
 
